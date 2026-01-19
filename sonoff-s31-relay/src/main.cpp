7bb3a17edab8315aa141cfcb45cbf177e39cb789
Geoffrey Abulencia <geoffrey.abulencia@gmail.com>
2026-01-19 19:18:55 +0800

    Add working Sonoff S31 ESP-NOW receiver code

--- sonoff-s31-relay/src/main.cpp
+++ sonoff-s31-relay/src/main.cpp
@@ -1,55 +1,29 @@
-#include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266mDNS.h>
+#include <espnow.h>
 
-const char* ssid = "ZTE_2.4G_jT5nRx_EXT";
-const char* password = "4kjL9URf";
+#define RELAY_PIN 12
 
-#define RELAY_PIN 12   
+typedef struct struct_message {
+  float temperature;
+  bool humidifierState;
+} struct_message;
 
-ESP8266WebServer server(80);
+struct_message incomingData;
 
-// Logical control functions
-void humidifierOn() {
-  digitalWrite(RELAY_PIN, HIGH);   
-  server.send(200, "text/plain", "Humidifier ON");
-}
+void OnDataRecv(uint8_t *mac, uint8_t *incomingDataRaw, uint8_t len) {
+  memcpy(&incomingData, incomingDataRaw, sizeof(incomingData));
 
-void humidifierOff() {
-  digitalWrite(RELAY_PIN, LOW);  
-  server.send(200, "text/plain", "Humidifier OFF");
-}
-
-void handleRoot() {
-  server.send(200, "text/plain", "Sonoff S31 Humidifier Controller");
+  digitalWrite(RELAY_PIN, incomingData.humidifierState ? HIGH : LOW);
 }
 
 void setup() {
   pinMode(RELAY_PIN, OUTPUT);
-  humidifierOff();   // Start safely OFF
+  digitalWrite(RELAY_PIN, LOW);
 
-  Serial.begin(115200);
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-
-  if (MDNS.begin("humidifier")) {
-    Serial.println("mDNS started: http://humidifier.local");
-  }
-
-  // URL routes
-  server.on("/", handleRoot);
-  server.on("/on", humidifierOn);    // Logical ON
-  server.on("/off", humidifierOff);  // Logical OFF
-
-  server.begin();
+  WiFi.mode(WIFI_STA);
+  esp_now_init();
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
+  esp_now_register_recv_cb(OnDataRecv);
 }
 
-void loop() {
-  server.handleClient();
-  MDNS.update();
-}
+void loop() {}
